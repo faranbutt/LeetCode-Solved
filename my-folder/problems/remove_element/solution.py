87770eb0ccf64a199c34e02b9d1c0da8f1385293
Faran Butt <66089079+faranbutt@users.noreply.github.com>
2023-11-19 15:27:11 +0000

    Sync LeetCode submission - Remove Element (python3)

---
+++ my-folder/problems/remove_element/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k=0
+        for r in range(0,len(nums)):
+            if nums[r] != val:
+                nums[k] = nums[r]
+                k+=1
+        return k
